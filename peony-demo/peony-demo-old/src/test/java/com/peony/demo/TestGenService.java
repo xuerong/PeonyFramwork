e1fa5ca6d2ab63286e40365b2095b6446e1492f3
z30001651 <zhengyuzhen1@huawei.com>
2020-09-04 18:06:53 +0800

    项目构建完善

--- peony-demo/peony-demo-old/src/test/java/com/peony/demo/TestGenService.java
+++ peony-demo/peony-demo-old/src/test/java/com/peony/demo/TestGenService.java
@@ -21,49 +21,51 @@ public class TestGenService {
 
         // 只处理 当前类定义的 public 的方法
 
-        CtClass ctClazz = pool.get(serviceClass.getName());
-        CtClass cls = pool.makeClass(ctClazz.getName() + "$Proxy", ctClazz);
+//        CtClass ctClazz = pool.get(serviceClass.getName());
+//        CtClass cls = pool.makeClass(ctClazz.getName() + "$Proxy", ctClazz);
+//
+//        // 改写remote方法, 使用远程调用
+//        for(CtMethod ctMethod:ctClazz.getMethods()) {
+//            // 重写该方法
+//            StringBuilder sb = new StringBuilder("public "+ctMethod.getReturnType().getName() + " " + ctMethod.getName() + "(");
+//            CtClass[] paramClasses = ctMethod.getParameterTypes();
+//            int i = 0;
+//            StringBuilder paramsStr = new StringBuilder();
+//            for (CtClass paramClass : paramClasses) {
+//                if (i > 0) {
+//                    sb.append(",");
+//                    paramsStr.append(",");
+//                }
+//                sb.append(paramClass.getName() + " p" + i);
+//                // 这个地方需要进行一步强制转换,基本类型不能编译成Object类型
+//                paramsStr.append(parseBaseTypeStrToObjectTypeStr(paramClass.getName(), "p" + i));
+//                i++;
+//            }
+//            sb.append(") {");
+//            String paramStr = "null";
+//            if (i > 0) {
+//                sb.append("Object[] param = new Object[]{" + paramsStr + "};");
+//                paramStr = "param";
+//            }
+//            // --------------这个地方要进行强制转换
+//            sb.append("com.peony.core.control.netEvent.remote.RemoteCallService remoteCallService = (com.peony.core.control.netEvent.remote.RemoteCallService)com.peony.core.control.BeanHelper.getServiceBean(com.peony.core.control.netEvent.remote.RemoteCallService.class);");
+//            String invokeStr = "remoteCallService.remoteCallMainServerSyn(" + serviceClass.getName() + ".class,\"" + ctMethod.getName() + "\"," + paramStr + ");";
+//            if (ctMethod.getReturnType().getName().toLowerCase().equals("void")) {
+//                sb.append(invokeStr);
+//            } else {
+//                sb.append("Object object = " + invokeStr);
+//                sb.append("return " + parseBaseTypeStrToObjectTypeStr(ctMethod.getReturnType().getName()));
+//            }
+//            sb.append("}");
+////            log.info("==============================================\n"+sb.toString());
+//            System.out.println(sb);
+//            CtMethod method = CtMethod.make(sb.toString(), cls);
+//            cls.addMethod(method);
+//        }
+//        newServiceClass = cls.toClass();
+//        return newServiceClass;
 
-        // 改写remote方法, 使用远程调用
-        for(CtMethod ctMethod:ctClazz.getMethods()) {
-            // 重写该方法
-            StringBuilder sb = new StringBuilder("public "+ctMethod.getReturnType().getName() + " " + ctMethod.getName() + "(");
-            CtClass[] paramClasses = ctMethod.getParameterTypes();
-            int i = 0;
-            StringBuilder paramsStr = new StringBuilder();
-            for (CtClass paramClass : paramClasses) {
-                if (i > 0) {
-                    sb.append(",");
-                    paramsStr.append(",");
-                }
-                sb.append(paramClass.getName() + " p" + i);
-                // 这个地方需要进行一步强制转换,基本类型不能编译成Object类型
-                paramsStr.append(parseBaseTypeStrToObjectTypeStr(paramClass.getName(), "p" + i));
-                i++;
-            }
-            sb.append(") {");
-            String paramStr = "null";
-            if (i > 0) {
-                sb.append("Object[] param = new Object[]{" + paramsStr + "};");
-                paramStr = "param";
-            }
-            // --------------这个地方要进行强制转换
-            sb.append("com.peony.core.control.netEvent.remote.RemoteCallService remoteCallService = (com.peony.core.control.netEvent.remote.RemoteCallService)com.peony.core.control.BeanHelper.getServiceBean(com.peony.core.control.netEvent.remote.RemoteCallService.class);");
-            String invokeStr = "remoteCallService.remoteCallMainServerSyn(" + serviceClass.getName() + ".class,\"" + ctMethod.getName() + "\"," + paramStr + ");";
-            if (ctMethod.getReturnType().getName().toLowerCase().equals("void")) {
-                sb.append(invokeStr);
-            } else {
-                sb.append("Object object = " + invokeStr);
-                sb.append("return " + parseBaseTypeStrToObjectTypeStr(ctMethod.getReturnType().getName()));
-            }
-            sb.append("}");
-//            log.info("==============================================\n"+sb.toString());
-            System.out.println(sb);
-            CtMethod method = CtMethod.make(sb.toString(), cls);
-            cls.addMethod(method);
-        }
-        newServiceClass = cls.toClass();
-        return newServiceClass;
+        return null;
     }
 
 
